25c7f70da7e9bdc63a3db90692bce7e6fc93f418
una3325 <115125675+una3325@users.noreply.github.com>
2025-05-24 16:32:29 +0900

    [level 1] Title: 카드 뭉치, Time: 0.13 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.js
@@ -0,0 +1,13 @@
+function solution(cards1, cards2, goal) {
+    while (goal.length > 0) {
+        if (cards1[0] === goal[0]) {
+            cards1.shift();
+            goal.shift();
+        } else if (cards2[0] === goal[0]) {
+            cards2.shift();
+            goal.shift();
+        } else {
+            return "No";
+        }
+    } return "Yes"
+}

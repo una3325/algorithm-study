9718ddba862d820ca1c934ef4df80f1055fde8e5
una3325 <115125675+una3325@users.noreply.github.com>
2025-06-02 17:30:10 +0900

    [level 2] Title: 주식가격, Time: 34.43 ms, Memory: 44.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/42584. 주식가격/주식가격.js
@@ -0,0 +1,19 @@
+function solution(prices) {
+    var answer = [];
+    
+    for(let i=0; i<prices.length; i++){
+        let cnt = 0
+        for(let j=i+1; j<prices.length; j++){
+            if(prices[i]<=prices[j]){
+                cnt++;   
+            }
+            if(prices[i]>prices[j]){
+                cnt ++;
+                break;
+            }
+        }
+        answer.push(cnt)
+    }
+    
+    return answer;
+} 

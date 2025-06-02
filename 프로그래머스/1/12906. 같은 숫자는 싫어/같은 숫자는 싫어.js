324dc98b3abcd5d56fe4eb633f0bdccc526f54fb
una3325 <115125675+una3325@users.noreply.github.com>
2025-06-03 01:15:22 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 41.86 ms, Memory: 82.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -0,0 +1,11 @@
+function solution(arr)
+{
+    var answer = [];
+    for(let i=0; i<arr.length; i++){
+        if(answer.length > 0 && answer[answer.length-1] == arr[i]){
+            answer.pop()
+        }
+        answer.push(arr[i])
+    }
+    return answer;
+}
